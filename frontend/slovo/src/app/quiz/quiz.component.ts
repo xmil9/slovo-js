2d66911ef910e30b411933b30ec082bf0a193d03
Mike Lindner <mlindner9@yahoo.com>
2024-09-08 19:59:23 -0400

    Quiz logic and unstyled ui.

--- frontend/slovo/src/app/quiz/quiz.component.ts
+++ frontend/slovo/src/app/quiz/quiz.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { NoWord, Word, WordId, WordService } from '../word.service';
-import { concatMap, iif, Observable, of, tap } from 'rxjs';
+import { concatMap, iif, of, tap } from 'rxjs';
 import { randomInt } from '../util';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 function calcRandomWordId(numWords: number): WordId {
 	return <WordId>(randomInt(numWords) + 1)
@@ -10,13 +14,17 @@ function calcRandomWordId(numWords: number): WordId {
 @Component({
 	selector: 'app-quiz',
 	standalone: true,
-	imports: [],
+	imports: [ FormsModule, MatCardModule, MatFormFieldModule, NgIf ],
 	templateUrl: './quiz.component.html',
 	styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
 	private numWords = 0;
 	word = NoWord;
+	translation = '';
+	isCorrect = false;
+	isEvaluated = false;
+	isRevealed = false;
 	
 	constructor(private wordService: WordService) { }
 
@@ -24,7 +32,25 @@ export class QuizComponent {
 		this.updateWord();
 	}
 
-	updateWord(): void {
+	next() {
+		this.translation = '';
+		this.clearEvaluate();
+		this.clearReveal();
+		this.updateWord();
+	}
+
+	evaluate() {
+		this.clearReveal();
+		this.isEvaluated = true;
+		this.isCorrect = this.translation === this.word.ru;
+	}
+
+	reveal() {
+		this.clearEvaluate();
+		this.isRevealed = true;
+	}
+	
+	private updateWord(): void {
 		const wordCount$ = iif(
 			() => this.numWords > 0,
 			of(this.numWords),
@@ -34,6 +60,15 @@ export class QuizComponent {
 		);
 		wordCount$.pipe(
 			concatMap((count: number) => this.wordService.getWord(calcRandomWordId(count)))
-		).subscribe((word: Word) => this.word = word );
+		).subscribe((word: Word) => this.word = word);
+	}
+	
+	private clearEvaluate() {
+		this.isEvaluated = false;
+		this.isCorrect = false;
+	}
+	
+	private clearReveal() {
+		this.isRevealed = false;
 	}
 }
