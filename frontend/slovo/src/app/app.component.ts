ff187f1c65aadea95c8858a48e1e98d8c0ed509c
Mike Lindner <mlindner9@yahoo.com>
2024-10-05 14:36:26 -0400

    Add sidenav support.

--- frontend/slovo/src/app/app.component.ts
+++ frontend/slovo/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, ToolbarComponent ],
+  imports: [ MatSidenavModule, RouterOutlet, ToolbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
