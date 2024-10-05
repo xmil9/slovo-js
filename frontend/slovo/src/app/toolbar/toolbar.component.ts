71cd4bc6dd8cf618444a7582531a797fec650f1d
Mike Lindner <mlindner9@yahoo.com>
2024-10-05 16:58:24 -0400

    Link app button to word-list endpoint.

--- frontend/slovo/src/app/toolbar/toolbar.component.ts
+++ frontend/slovo/src/app/toolbar/toolbar.component.ts
@@ -2,17 +2,14 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-toolbar',
-  standalone: true,
-  imports: [ MatButtonModule, MatIconModule, MatToolbarModule ],
-  templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+	selector: 'app-toolbar',
+	standalone: true,
+	imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
+	templateUrl: './toolbar.component.html',
+	styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
-
-	onAppIconClicked() {
-		// todo - navigate to default route
-	}
 }
