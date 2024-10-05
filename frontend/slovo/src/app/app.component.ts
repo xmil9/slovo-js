e68fd0b5e93ed3913ab7b23f9e0fa302ec3e4053
Mike Lindner <mlindner9@yahoo.com>
2024-10-05 16:55:00 -0400

    Sidenav with navigation.

--- frontend/slovo/src/app/app.component.ts
+++ frontend/slovo/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidenavComponent } from "./sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ MatSidenavModule, RouterOutlet, ToolbarComponent ],
+  imports: [MatSidenavModule, RouterOutlet, ToolbarComponent, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
